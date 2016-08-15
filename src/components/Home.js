db5be940f674355468b561b641470c7f66acd97e
Hyein <hyeinu65@gmail.com>
2016-08-15 11:38:58 -0700

    working on displaying totals on top

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,22 +8,27 @@ const Home = React.createClass({
   getInitialState(){
     try{
         var items = JSON.parse(localStorage.productlist)
+        let totalPrice = items.reduce((item, next) =>{
+          console.log("item.price:", item.price)
+          return parseInt(item.price) + parseInt(next.price);
+        }, 0)
       } catch(err){
         var items = [];
       }
+
     return{
       productlist: items,
       restaurant: {},
       addShow: false,
       sortName: true,
       sortPrice: true,
-      totalPrice: 0,
-      totalItems: 0
+      totalPrice: totalPrice,
+      totalItems: items.length
     }
   },
   totalPrice(){
     let price = this.state.productlist.reduce((item,next)=>{
-      return item.price + next.price
+      return parseInt(item.price) + parseInt(next.price);
     },0)
     this.setState({totalPrice: price});
   },
@@ -52,22 +57,58 @@ const Home = React.createClass({
   },
   sortName(){
     //sort the array according to name
+    let sortArr
       if(this.state.sortName){
-        let sortArr = this.state.productlist.sort((a,b) =>{
-          return a.name - b.name;
-          this.setState({sortName: false})
-          console.log("sortArr:", sortArr)
+        sortArr = this.state.productlist.sort((a,b) =>{
+          if(a.name < b.name){
+            return -1;
+          } else if(a.name > b.name){
+            return 1;
+          } else {
+            return 0;
+          }
         });
+        this.setState({sortName: false})
       }else{
-        let sortArr = this.state.productlist.sort((a,b) =>{
-          return b.name - a.name;
+        sortArr = this.state.productlist.sort((a,b) =>{
+          if(b.name < a.name){
+            return -1;
+          } else if(b.name > a.name){
+            return 1;
+          } else {
+            return 0;
+          }
         });
+        this.setState({sortName: true})
       }
-
-    // this.setState({productlist: sortArr})
+      this.setState({productlist: sortArr})
   },
   sortPrice(){
-  
+    let sortArr
+      if(this.state.sortPrice){
+        sortArr = this.state.productlist.sort((a,b) =>{
+          if(a.price < b.price){
+            return -1;
+          } else if(a.price > b.price){
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        this.setState({sortPrice: false})
+      }else{
+        sortArr = this.state.productlist.sort((a,b) =>{
+          if(b.price < a.price){
+            return -1;
+          } else if(b.price > a.price){
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+      this.setState({sortPrice: true})
+      }
+      this.setState({productlist: sortArr})
   },
   componentDidUpdate(){
   localStorage.productlist = JSON.stringify(this.state.productlist);
@@ -78,11 +119,12 @@ const Home = React.createClass({
         <div>
         <div className="row">
           <div className="col-xs-8">
-          <h2>Add New Product</h2>
+          <h2>Product List</h2>
+          <h4>Total Items: {this.state.totalItems} || Total Price: {this.state.totalPrice}</h4>
           </div>
           <div className="col-xs-4">
           <br />
-          <Button onClick={this.openAddModal} className="btn-success fa fa-plus-square fa-sm"></Button>
+          <h4>New <Button onClick={this.openAddModal} className="btn-success fa fa-plus-square fa-sm"></Button></h4>
           </div>
         </div>
         <ProductTable productlist={this.state.productlist} update={this.updateItem} delete={this.deleteItem} sortName={this.sortName} sortPrice={this.sortPrice}/>
